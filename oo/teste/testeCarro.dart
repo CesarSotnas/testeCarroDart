6a21a4504c4965bae89cc37d5b4fb3df3770f06a
Cesar <devcarlos.cesar@gmail.com>
2020-07-22 10:07:55 -0300

    Exercicio em Dart

---
+++ oo/teste/testeCarro.dart
@@ -0,0 +1,19 @@
+import "../modelo/modeloCarro.dart";
+
+main() {
+  var c1 = new Carro(320); 
+
+  while(!c1.estaNoLimite()){
+    print("A velocidade atual é ${c1.acelerar()} Km/h.");
+  }
+
+  print("O carro chegou no máximo com velocidade ${c1.velocidadeAtual} Km/h.");
+
+  while(c1.velocidadeAtual > 0 ){
+    print("A velocidade atual é ${c1.frear()} Km/h.");
+  }
+
+  print("O carro parou com velocidade ${c1.velocidadeAtual} Km/h.");
+
+
+}
